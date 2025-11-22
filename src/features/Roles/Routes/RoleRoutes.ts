6b007681f42e44da217ad3516eeb2ea65c8a36df
Abhishek N <abhishekpes123@gmail.com>
2025-11-22 13:50:45 +0530

    latest-commit

--- src/features/Roles/Routes/RoleRoutes.ts
+++ src/features/Roles/Routes/RoleRoutes.ts
@@ -8,7 +8,7 @@ const router=express.Router();
 router.post('/create',protect,CreateRoleController);
 router.get('/all',protect,GetRoleController);
 router.get('/:id',protect,GetRoleByIdController);
-router.put('/update',protect,UpdateRoleController);
+router.put('/:id/update',protect,UpdateRoleController);
 router.delete('/:id/delete',protect,DeleteRoleController);
 
 export default router;
